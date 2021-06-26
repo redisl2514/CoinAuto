051780fbc09519719b0feeceb18f8f972a64e5f5
redisl2514 <83148695+redisl2514@users.noreply.github.com>
2021-06-26 20:34:00 +0900

    Update Upbit_auto.py

--- Upbit_auto.py
+++ Upbit_auto.py
@@ -1,59 +1,89 @@
-import time
 import pyupbit
-import datetime
+import random
+import time
+import requests
+
+def post_message(token, channel, text):
+    response = requests.post("https://slack.com/api/chat.postMessage",
+                             headers={"Authorization": "Bearer " + token},
+                             data={"channel": channel, "text": text})
+    #print(response)
+
+myToken = "xoxb-2214036150211-2213836848130-MGOLLe5LfbVZP5h8buojkHCY"
+post_message(myToken, "#stock", "코인 자동매매 시작")
+
+#def dbgout(message):
+#    """인자로 받은 문자열을 파이썬 셸과 슬랙으로 동시에 출력한다."""
+#    print(datetime.now().strftime('[%m/%d %H:%M:%S]'), message)
+#    strbuf = datetime.now().strftime('[%m/%d %H:%M:%S] ') + message
+#    post_message(myToken,"#stock", strbuf)
 
 access = "W0pBEN1VwvBvTHtp8zD2hVfgxufsv0PHzrgwCbaS"
 secret = "z0iST1K4uhFwP1QKX8riwXmIPxwRsPu4xnUr9KKw"
 
-def get_target_price(ticker, k):
-    """변동성 돌파 전략으로 매수 목표가 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
-    target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
-    return target_price
+tickers = pyupbit.get_tickers(fiat="KRW") #업비트에 있는 원화코인만 검색
+random.shuffle(tickers) ## ticker 랜덤으로 Scan 하도록 명령.
 
-def get_start_time(ticker):
-    """시작 시간 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
-    start_time = df.index[0]
-    return start_time
+#post_message(myToken, "#stock", "종목:" + str(tickers) + "매수 완료")
+#post_message(myToken, "#stock", "종목:" + str(tickers) + "매도 완료")
 
-def get_balance(ticker):
-    """잔고 조회"""
-    balances = upbit.get_balances()
-    for b in balances:
-        if b['currency'] == ticker:
-            if b['balance'] is not None:
-                return float(b['balance'])
-            else:
-                return 0
+def get_target_price(ticker):
+    """종목 5분봉 현재 이전 종가 가격"""
+    Y_lp_ma5 = pyupbit.get_ohlcv(ticker, interval="minute5")
+    Y_lm5_price = Y_lp_ma5['close'].rolling(5).mean()
+    Y_lm5_list = Y_lm5_price.iloc[-2]
+    return Y_lm5_list
 
 def get_current_price(ticker):
-    """현재가 조회"""
-    return pyupbit.get_orderbook(tickers=ticker)[0]["orderbook_units"][0]["ask_price"]
+    """종목 5분봉 현재 가격"""
+    Now_lp_ma5 = pyupbit.get_ohlcv(ticker, interval="minute5")
+    Now_lm5_price = Now_lp_ma5['close'].rolling(5).mean()
+    Now_lm5_list = Now_lm5_price.iloc[-1]
+    return Now_lm5_list
+
+def sell_target_price(ticker):
+    """종목 5분봉 10일선 현재 가격"""
+    Now_lp_ma10 = pyupbit.get_ohlcv(ticker, interval="minute5")
+    Now_lm10_price = Now_lp_ma10['close'].rolling(10).mean()
+    Now_lm10_list = Now_lm10_price.iloc[-1]
+    return Now_lm10_list
+
+def buy_crypto_currency(ticker):
+    krw = upbit.get_balance(ticker="KRW")  # 잔고에 남아있는 돈 얻어옴
+    buy_price = pyupbit.get_current_price(ticker) #종목 현재가(매수가)
+    unit = krw / float(buy_price) #원화 잔고를 최우선 매도가로 나눠서 구매 가능한 수량을 계산
+    unit2 = unit - unit * 0.05 #주문 수량에서 수수료(unit * 0.015) 뺀 가격
+    upbit.buy_limit_order(ticker, buy_price, unit2) #종목, 매수가, 매수 수량
+    #upbit.buy_market_order(ticker, krw)
+    #print("%s %s개 매수" % ticker, unit)
+
+def sell_crypto_currency(ticker):
+    unit = upbit.get_balance(ticker)
+    upbit.sell_market_order(ticker, unit)
 
 # 로그인
 upbit = pyupbit.Upbit(access, secret)
-print("autotrade start")
+print("자동매매 시작")
 
-# 자동매매 시작
 while True:
-    try:
-        now = datetime.datetime.now()
-        start_time = get_start_time("KRW-STPT")
-        end_time = start_time + datetime.timedelta(days=1)
-
-        if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-STPT", 0.5)
-            current_price = get_current_price("KRW-STPT")
+    for ticker in tickers:
+        target_price = get_target_price(ticker) + get_target_price(ticker) * 0.02
+        current_price = get_current_price(ticker)
+        sell_price = sell_target_price(ticker)
+        try:
             if target_price < current_price:
-                krw = get_balance("KRW")
-                if krw > 5000:
-                    upbit.buy_market_order("KRW-STPT", krw*0.9995)
-        else:
-            sell_unit = get_balance("KRW")    #btc = get_balance("BTC")
-            if sell_unit > 5000:
-                upbit.sell_market_order("KRW-STPT", ticker*0.9995)
-        time.sleep(1)
-    except Exception as e:
-        print(e)
-        time.sleep(1)
+                buy_crypto_currency(ticker)
+                print("종목 :", ticker)
+                print("매수 완료")
+                post_message(myToken, "#stock", "종목:" + str(ticker) + "매수 완료")
+            else:
+                if current_price < sell_price:
+                    sell_crypto_currency(ticker)
+                    print("종목 :", ticker)
+                    print("매도 완료")
+                    post_message(myToken, "#stock", "종목:" + str(ticker) + "매도 완료")
+        except:
+            time.sleep(0.5)
+    time.sleep(1)
+    print("-----------------------잠시 대기 중-----------------------")
+    #post_message(myToken, "#stock", "잠시 대기 중")
